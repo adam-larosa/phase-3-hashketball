c571e2762ec0c94dee322eb79d2ca6bf2606a6b6
Adam La Rosa <adam.larosa@flatironschool.com>
2021-12-14 09:25:52 -0800

    solution

--- hashketball.rb
+++ hashketball.rb
@@ -1,129 +1,194 @@
 # Write your code below game_hash
+require 'pry'
+
 def game_hash
-  {
-    home: {
-      team_name: "Brooklyn Nets",
-      colors: ["Black", "White"],
-      players: [
-        {
-          player_name: "Alan Anderson",
-          number: 0,
-          shoe: 16,
-          points: 22,
-          rebounds: 12,
-          assists: 12,
-          steals: 3,
-          blocks: 1,
-          slam_dunks: 1
-        },
-        {
-          player_name: "Reggie Evans",
-          number: 30,
-          shoe: 14,
-          points: 12,
-          rebounds: 12,
-          assists: 12,
-          steals: 12,
-          blocks: 12,
-          slam_dunks: 7
-        },
-        {
-          player_name: "Brook Lopez",
-          number: 11,
-          shoe: 17,
-          points: 17,
-          rebounds: 19,
-          assists: 10,
-          steals: 3,
-          blocks: 1,
-          slam_dunks: 15
-        },
-        {
-          player_name: "Mason Plumlee",
-          number: 1,
-          shoe: 19,
-          points: 26,
-          rebounds: 11,
-          assists: 6,
-          steals: 3,
-          blocks: 8,
-          slam_dunks: 5
-        },
-        {
-          player_name: "Jason Terry",
-          number: 31,
-          shoe: 15,
-          points: 19,
-          rebounds: 2,
-          assists: 2,
-          steals: 4,
-          blocks: 11,
-          slam_dunks: 1
-        }
-      ]
-    },
-    away: {
-      team_name: "Charlotte Hornets",
-      colors: ["Turquoise", "Purple"],
-      players: [
-        {
-          player_name: "Jeff Adrien",
-          number: 4,
-          shoe: 18,
-          points: 10,
-          rebounds: 1,
-          assists: 1,
-          steals: 2,
-          blocks: 7,
-          slam_dunks: 2
-        },
-        {
-          player_name: "Bismack Biyombo",
-          number: 0,
-          shoe: 16,
-          points: 12,
-          rebounds: 4,
-          assists: 7,
-          steals: 22,
-          blocks: 15,
-          slam_dunks: 10
-        },
-        {
-          player_name: "DeSagna Diop",
-          number: 2,
-          shoe: 14,
-          points: 24,
-          rebounds: 12,
-          assists: 12,
-          steals: 4,
-          blocks: 5,
-          slam_dunks: 5
-        },
-        {
-          player_name: "Ben Gordon",
-          number: 8,
-          shoe: 15,
-          points: 33,
-          rebounds: 3,
-          assists: 2,
-          steals: 1,
-          blocks: 1,
-          slam_dunks: 0
-        },
-        {
-          player_name: "Kemba Walker",
-          number: 33,
-          shoe: 15,
-          points: 6,
-          rebounds: 12,
-          assists: 12,
-          steals: 7,
-          blocks: 5,
-          slam_dunks: 12
-        }
-      ]
-    }
-  }
+	{
+		home: {
+			team_name: "Brooklyn Nets",
+			colors: ["Black", "White"],
+			players: [
+				{
+					player_name: "Alan Anderson",
+					number: 0,
+					shoe: 16,
+					points: 22,
+					rebounds: 12,
+					assists: 12,
+					steals: 3,
+					blocks: 1,
+					slam_dunks: 1
+				},
+				{
+					player_name: "Reggie Evans",
+					number: 30,
+					shoe: 14,
+					points: 12,
+					rebounds: 12,
+					assists: 12,
+					steals: 12,
+					blocks: 12,
+					slam_dunks: 7
+				},
+				{
+					player_name: "Brook Lopez",
+					number: 11,
+					shoe: 17,
+					points: 17,
+					rebounds: 19,
+					assists: 10,
+					steals: 3,
+					blocks: 1,
+					slam_dunks: 15
+				},
+				{
+					player_name: "Mason Plumlee",
+					number: 1,
+					shoe: 19,
+					points: 26,
+					rebounds: 11,
+					assists: 6,
+					steals: 3,
+					blocks: 8,
+					slam_dunks: 5
+				},
+				{
+					player_name: "Jason Terry",
+					number: 31,
+					shoe: 15,
+					points: 19,
+					rebounds: 2,
+					assists: 2,
+					steals: 4,
+					blocks: 11,
+					slam_dunks: 1
+				}
+			]
+		},
+		away: {
+			team_name: "Charlotte Hornets",
+			colors: ["Turquoise", "Purple"],
+			players: [
+				{
+					player_name: "Jeff Adrien",
+					number: 4,
+					shoe: 18,
+					points: 10,
+					rebounds: 1,
+					assists: 1,
+					steals: 2,
+					blocks: 7,
+					slam_dunks: 2
+				},
+				{
+					player_name: "Bismack Biyombo",
+					number: 0,
+					shoe: 16,
+					points: 12,
+					rebounds: 4,
+					assists: 7,
+					steals: 22,
+					blocks: 15,
+					slam_dunks: 10
+				},
+				{
+					player_name: "DeSagna Diop",
+					number: 2,
+					shoe: 14,
+					points: 24,
+					rebounds: 12,
+					assists: 12,
+					steals: 4,
+					blocks: 5,
+					slam_dunks: 5
+				},
+				{
+					player_name: "Ben Gordon",
+					number: 8,
+					shoe: 15,
+					points: 33,
+					rebounds: 3,
+					assists: 2,
+					steals: 1,
+					blocks: 1,
+					slam_dunks: 0
+				},
+				{
+					player_name: "Kemba Walker",
+					number: 33,
+					shoe: 15,
+					points: 6,
+					rebounds: 12,
+					assists: 12,
+					steals: 7,
+					blocks: 5,
+					slam_dunks: 12
+				}
+			]
+		}
+	}
 end
 
+def get_player(name)
+	game_hash.values.each {|team|
+		team[:players].each {|player|
+			if player[:player_name] == name
+				return player
+			end
+		}
+	}
+end
+
+
+def num_points_scored(name)
+	get_player(name)[:points]
+end
+
+def shoe_size(name)
+	get_player(name)[:shoe]
+end
+
+def team_colors(name_of_team)
+	game_hash.values.each { |team|
+		if team[:team_name] == name_of_team
+			return team[:colors]
+		end
+	}
+end
+
+def team_names
+	game_hash.values.map {|team| team[:team_name]}
+end
+
+def player_numbers(name_of_team)
+	game_hash.values.each {|team|
+		if team[:team_name] == name_of_team
+			return team[:players].map {|player| player[:number]}
+		end
+	}
+end
+
+def player_stats(player_name)
+	get_player(player_name)
+end
+
+def big_shoe_rebounds
+	# look through all the players
+	# check to see what size shoe they have
+	# if the shoe size is larger than those found before, remember it!
+	# once we're done looking at all the players, return the rebounds for the
+	# found player
+
+	the_brain = {shoe_size: 0, player: nil}
+
+	game_hash.values.each {|team|
+		team[:players].each {|player|
+			if player[:shoe] > the_brain[:shoe_size]
+				the_brain[:shoe_size] = player[:shoe]
+				the_brain[:player] = player
+			end
+		}
+	}
+	the_brain[:player][:rebounds]
+
+end
 # Write code here
